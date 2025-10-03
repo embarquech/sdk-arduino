d83a0105a9685b597a7d15abd408e08c05f54742
Dan Pham <dan@danpham.com>
2025-10-03 11:36:16 +0200

    Python file with pep8 errors.

---
+++ test.py
@@ -0,0 +1,12 @@
+# fichier test_pep8.py
+
+def ma_fonction(  a,b ):
+  print( "Bonjour le monde" )
+  if(a>0):
+     print("a est positif")
+  else:
+      print("a est négatif ou nul")  
+
+x=  5
+y=10
+ma_fonction( x ,y )
